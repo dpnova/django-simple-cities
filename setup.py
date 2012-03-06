0a9169d8cc037d5fed9a9ecbd1b8596e33b9fd4a
David Novakovic <davidnovakovic@gmail.com>
2012-03-06 11:20:55 +1000

    Add a bit of detail to the setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+rom setuptools import setup, find_packages
+
+setup(name="django-simple-cities",
+           version="0.1",
+           description="Simple City DB for Django, includes Ajax select filtering.",
+           author="David Novakovic",
+           packages=find_packages(),
+           include_package_data=True,
+)
